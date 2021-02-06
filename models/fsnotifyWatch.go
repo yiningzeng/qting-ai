ab66c7d8968a68fd67eacd53c0a06971f0e3ad8a
baymin <yiningzeng@outlook.com>
2021-02-06 12:36:45 +0800

    feat: 新增的模型支持名称和自增轻蜓的记录

--- models/fsnotifyWatch.go
+++ models/fsnotifyWatch.go
@@ -212,27 +212,31 @@ func QTingTiny3l(taskId string, modelPath string) {
 		qtAiFramework, _ := GetQtAiFrameworkByFrameworkName(aiFrameworkName)
 		var tempName = ""
 		// 先查询是否存在相关的训练记录
-		//if qtTrainRecord, err := GetQtTrainRecordByTaskId(taskId); err == nil {
-		//	tempName = "(" + qtTrainRecord.TaskName + ")"
-		//} else {
-		//	tempName = "(轻蜓训练)"
-		//	// 接下去插入记录
-		//	_, err = AddQtTrainRecord(&QtTrainRecord{
-		//		TaskId:        taskId,
-		//		ContainerId:	"",
-		//		TaskName:      "自动新增(轻蜓训练)",
-		//		ProjectId:     qtProject,
-		//		Status:        4,
-		//		AiFrameworkId: qtAiFramework,
-		//		AssetsType:    "",
-		//		IsJump:        0,
-		//		DrawUrl:       "",
-		//		CreateTime:    time.Now(),
-		//	})
-		//	if err !=nil{
-		//		logrus.Error(err.Error())
-		//	}
-		//}
+		taskIdTemp := ""
+		if strings.Contains(taskId, "-") {
+			taskIdTemp = strings.Split(taskId, "-")[0]
+		}
+		if qtTrainRecord, err := GetQtTrainRecordByTaskId(taskIdTemp); err == nil {
+			tempName = "(" + qtTrainRecord.TaskName + ")"
+		} else {
+			tempName = "(轻蜓训练)"
+			// 接下去插入记录
+			_, err = AddQtTrainRecord(&QtTrainRecord{
+				TaskId:        taskIdTemp,
+				ContainerId:	"",
+				TaskName:      "自动新增-轻蜓训练",
+				ProjectId:     qtProject,
+				Status:        4,
+				AiFrameworkId: qtAiFramework,
+				AssetsType:    "",
+				IsJump:        0,
+				DrawUrl:       "",
+				CreateTime:    time.Now(),
+			})
+			if err !=nil{
+				logrus.Error(err.Error())
+			}
+		}
 		// 插入模型数据
 		if qtModels, err := GetQtModelsByTaskId(taskId); err == nil {
 			qtModels.TaskId = taskId
@@ -247,7 +251,7 @@ func QTingTiny3l(taskId string, modelPath string) {
 				logrus.WithField("err", "qtingsl更新模型失败").Error(err.Error())
 			}
 		} else {
-			_, _ = AddQtModels(&QtModels{
+			_, err = AddQtModels(&QtModels{
 				TaskId:        taskId,
 				ProjectId:     qtProject,
 				AiFrameworkId: qtAiFramework,
@@ -256,6 +260,9 @@ func QTingTiny3l(taskId string, modelPath string) {
 				Status:        0,
 				CreateTime:    time.Now(),
 			})
+			if err != nil {
+				logrus.WithField("err", "新增模型失败").Error(err.Error())
+			}
 		}
 	} else {
 		logrus.Error(err)
