2b80c3d2c694ba50605ed86c31c2617689843375
baymin <yiningzeng@outlook.com>
2021-02-25 18:27:06 +0800

    feat: 更改标准的插件接口

--- plugins/QTing-tiny-3l/main.go
+++ plugins/QTing-tiny-3l/main.go
@@ -26,7 +26,9 @@ func Version(args ...interface{}) (ret interface{}, err error) {
 }
 
 // 插件通用的执行方法
-func Run(taskId string, modelPath string) (err error) {
+func Run(args ...interface{}) (err error) {
+	taskId := args[0].(string)
+	modelPath := args[1].(string)
 	temp := strings.Split(strings.ReplaceAll(modelPath, beego.AppConfig.DefaultString("ProjectPath", "/qtingvisionfolder/Projects/"), ""), "/")
 	projectName := temp[0]
 	aiFrameworkName := temp[2]
